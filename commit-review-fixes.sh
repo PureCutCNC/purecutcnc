b18c1ea9c55da003c11b1112e040e928976adb61
Frankie Doo <frank.povazanj@gmail.com>
2026-07-24 09:38:24 -0400

    chore: remove temporary commit script

--- commit-review-fixes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /Users/frankp/Projects/worktrees/purecutcnc/issue-356-exported-motion-debug
-git add -A
-git commit -m "fix: address PR review comments #2, #5, #7
-
-- Remove duplicate Reset button (identical to Fit)
-- Add captureMotionTrace test to postprocessor.test.ts
-- Add tablet 44px min-height for Z-level select element"
-git push
